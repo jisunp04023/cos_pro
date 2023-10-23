d721e3ef55c24fe9c452574e77b119236583bc9e
ferozsun <wltjswltjs7101@gmail.com>
2023-10-23 16:25:06 +0900

    Create 3-1_TurnArray.py

---
+++ 3-1_TurnArray.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+def func_a(arr):
+	ret = arr + arr
+	return ret
+
+def func_b(first, second):
+	MAX_NUMBER = 1001
+	counter = [0 for _ in range(MAX_NUMBER)]
+	for f, s in zip(first, second):
+		counter[f] += 1
+		counter[s] -= 1
+	for c in counter:
+		if c != 0:
+			return False
+	return True
+
+def func_c(first, second):
+	length = len(second)
+	for i in range(length):
+		if first[i : i + length] == second:
+			return True
+	return False
